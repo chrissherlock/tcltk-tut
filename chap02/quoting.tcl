275a24b9cfcd41549e0b8fa97ac42c8d103d1b5e
Christopher Sherlock <chris.sherlock79@gmail.com>
2025-11-16 01:01:58 +1100

    breaks vim syntax highlighting... how to use double quote

--- chap02/quoting.tcl
+++ chap02/quoting.tcl
@@ -9,12 +9,14 @@ set msg2 "Eggs: \$2.18/dozen
 Gasoline: \$1.49/gallon"
 puts $msg2
 
-
 # don't do this... you can put double-quotes in the middle
 # of words but it's not good practice
-puts This"is"poor"usage
+puts This"is"poor"usage"
 
 # show how substititions all occur inside double quotes
 set a 2.1
-set msg "a is $a; the square of a is [expr $a*$a]"
-puts $msg
+puts "a is $a; the square of a is [expr $a*$a]"
+
+# how to use a double quote
+set name a.out
+puts "Couldn't open file \"$name\""
