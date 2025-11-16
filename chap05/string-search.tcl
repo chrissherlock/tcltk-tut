ce0bfe6284ab51805f1c22fce1cd4ec8c987dff3
Christopher Sherlock <chris.sherlock79@gmail.com>
2025-11-16 23:02:48 +1100

    string search

---
+++ chap05/string-search.tcl
@@ -0,0 +1,21 @@
+#!/usr/bin/env tclsh
+
+puts [string index "Sample string" 3]
+# => p
+
+set i 1
+puts [string index "Sample string" end-$i]
+# => i
+
+puts [string index "Sample string" 5+$i]
+# => s
+
+incr i
+puts [string index "Sample string" 5+$i]
+# => t
+
+puts [string range "Sample string" 3 7]
+# => ple s
+
+puts [string range "Sample string" 3 end]
+# => ple string
