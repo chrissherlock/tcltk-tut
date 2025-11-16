4bb06dc43a5736461535f2487c0833ae0891cb24
Christopher Sherlock <chris.sherlock79@gmail.com>
2025-11-16 21:54:25 +1100

    more predefined variables

--- chap03/printargs.tcl
+++ chap03/printargs.tcl
@@ -2,3 +2,8 @@
 
 puts "The command name is \"argv0\""
 puts "There were $argc arguments: $argv"
+
+puts "Your home directory is $env(HOME)"
+puts $tcl_platform(platform)
+puts $tcl_platform(os)
+puts $tcl_platform(machine)
