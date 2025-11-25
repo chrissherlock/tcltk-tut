f979c562164b2e0369dfc4b42d9ead61fa9e5415
Christopher Sherlock <chris.sherlock79@gmail.com>
2025-11-25 21:09:59 +1100

    more examples

--- chap07/dict-update.tcl
+++ chap07/dict-update.tcl
@@ -36,3 +36,21 @@ set polyFactors {C 1 x 2 y 3}
 squareValue polyFactors y
 puts $polyFactors
 # => C 1 x 2 y 9
+
+puts $example
+
+dict update example surname s notes n initial i {
+    dict set example title Mrs
+    unset s
+    set n "have initial = [info exists i]"
+    # print the current contents of the dictionary
+    puts $example
+}
+# => firstname Ann surname Huan title Mrs
+
+# Get the contents of the variable after the dict update command has
+# completed; note it is different in several respects, but the value
+# for 'title' is unchanged because that key was not listed at the
+# start of the command
+puts $example
+# => firstname Ann title Mrs notes {have initial = 0}
